86bc91b1af320e61621862a0962e56ddb09c52cc
Lucas Almeida <joxsal01@easv365.dk>
2026-05-05 11:03:41 +0200

    Initial Commit

---
+++ src/components/testComponent.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const posts = ref([]);
+
+onMounted(async () => {
+  const response = await axios.get('http://businessdedk.local/wp-json/wp/v2/posts');
+  posts.value = response.data;
+});
+</script>
+
+<template>
+  <div v-for="post in posts" :key="post.id">
+    <h2 v-html="post.title.rendered"></h2>
+    <div v-html="post.excerpt.rendered"></div>
+  </div>
+</template>
