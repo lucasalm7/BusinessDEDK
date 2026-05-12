3240ff285a27b20ceed705d273961c84adb52556
Lucas Almeida <joxsal01@easv365.dk>
2026-05-12 17:38:10 +0200

    [bug fix] tailwing not working

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="bg-dark-blue text-white p-4">
+  <nav class="p-4">
     <router-link to="/">Home</router-link> | 
     <router-link to="/posts">Posts</router-link>
   </nav>
