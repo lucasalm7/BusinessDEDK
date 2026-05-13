fc6d807a75ebe7e0ae4428fed39ab33259706321
Lucas Almeida <joxsal01@easv365.dk>
2026-05-13 15:32:26 +0200

    [design]nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,56 @@
-<script>
+<script setup>
 import { ref } from 'vue'
 
-export default {
-  setup() {
-    const isDropdownOpen = ref(false)
-    return {
-      isDropdownOpen
-    }
-  }
-}
+const isDropdownOpen = ref(false)
 </script>
 
 <template>
-  <nav class="p-4">
-    <router-link to="/">Home</router-link> | 
-    <router-link to="/media">Media</router-link> | 
-    <router-link to="/network">Network</router-link> | 
-    <router-link to="/about">About</router-link> | 
-    <router-link to="/events">Events</router-link> | 
-    <router-link to="/advisors">Advisors</router-link> |
+  <nav class="grid grid-cols-12 gap-8 items-center py-6 px-8 bg-white pl-[5%] pr-[5%]">
     
-    <div class="inline-block relative">
-      <button @click="isDropdownOpen = !isDropdownOpen" class="px-4 py-2 bg-gray-200 rounded cursor-pointer">
-        More ▼
-      </button>
-      <div v-if="isDropdownOpen" class="dropdown-menu">
-        <router-link to="/become-member" @click="isDropdownOpen = false">Become a member</router-link>
-        <router-link to="/border-region" @click="isDropdownOpen = false">Border Region</router-link>
-        <router-link to="/blog" @click="isDropdownOpen = false">Blog</router-link>
-        <router-link to="/photogallery" @click="isDropdownOpen = false">Photogallery</router-link>
-        <router-link to="/resources" @click="isDropdownOpen = false">Resources</router-link>
+    <div class="col-span-2 flex items-center">
+      <img src="./assets/img/logo.png" alt="Business Region logo" class=" w-auto object-contain block" />
+    </div>
+
+    <div class="col-span-6 col-start-4 flex justify-between">
+      <router-link to="/media" class="navtext">Media</router-link>
+      <router-link to="/network" class="navtext">Network</router-link>
+      <router-link to="/events" class="navtext">Events</router-link>
+      <router-link to="/about" class="navtext">About</router-link>
+      
+      <div class="relative">
+        <button 
+          @click="isDropdownOpen = !isDropdownOpen" 
+          class="flex items-center navtext"
+        >
+          More <span class="ml-1 text-xs">▼</span>
+        </button>
+        
+        <div v-if="isDropdownOpen" 
+             class="absolute left-0 mt-4 w-48 bg-white border border-gray-100 shadow-xl rounded-md z-50">
+          <router-link to="/become-member" class="navdropdown">Become a member</router-link>
+          <router-link to="/border-region" class="navdropdown">Border Region</router-link>
+          <router-link to="/blog" class="navdropdown">Blog</router-link>
+          <router-link to="/photogallery" class="navdropdown">Photogallery</router-link>
+          <router-link to="/resources" class="navdropdown">Resources</router-link>
+        </div>
       </div>
     </div>
+
+    <div class="col-start-11 flex justify-center items-center text-gray-600">
+      <button class="hover:text-blue-900">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+        </svg>
+      </button>
+    </div>
+
+    <div class="col-start-12 flex items-center justify-center text-gray-600">
+      <button class="font-medium hover:text-blue-900">
+       EN <span class="ml-1 text-xs">▼</span>
+      </button>
+    </div>
+
   </nav>
+
   <router-view />
 </template>
-
-
-<style scoped>
-.dropdown-menu {
-  position: absolute;
-  top: 100%;
-  left: 0;
-  background-color: white;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-shadow: 0 2px 10px rgba(0,0,0,0.1);
-  min-width: 150px;
-  z-index: 1000;
-}
-
-.dropdown-menu a {
-  display: block;
-  padding: 10px 16px;
-  color: #333;
-  text-decoration: none;
-  border-bottom: 1px solid #f0f0f0;
-}
-
-.dropdown-menu a:last-child {
-  border-bottom: none;
-}
-
-.dropdown-menu a:hover {
-  background-color: #f9f9f9;
-}
-</style>
